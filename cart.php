9daf9bda35f588bccb65b07aff0f8a3bbb37530d
lauren shelton <53822969+laurensheltonn@users.noreply.github.com>
2020-03-12 10:29:24 -0500

    changes

--- cart.php
+++ cart.php
@@ -1,4 +1,22 @@
+<?php
+    require 'config.php';
+
+    $grand_total = 0;
+    $allItems = '';
+    $items = array ();
+
+    $sql = "SELECT CONCAT (product_name, '(',qty,')') AS ItemQty, total_price FROM cart";
+    $stmt = $conn->prpeare($sql);
+    $result = $stmt->get_result();
+    while ($row = $result->fetchassoc()){
+        $grand_total +=$row['total_price'];
+        $items[] = $row['ItemQty'];
+     }
+     $alllItems = implode(",", $items);
+?>
+
 <?php include 'includes/header.php';?>
+
 
 
 
